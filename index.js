8e519c340108c664fd4f6e3123332341ede54622
Juan Mateos <juan.a.mateos@outlook.com>
2022-03-05 17:41:47 -0700

    test animation

---
+++ index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('web_development'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  })
